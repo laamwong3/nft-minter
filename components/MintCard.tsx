91693761cdc312927e4ddd5b77c4330f3d561171
Laam Wong <laam@Laams-iMac.local>
2022-07-09 07:48:57 +0800

    created listener

--- components/MintCard.tsx
+++ components/MintCard.tsx
@@ -8,23 +8,114 @@ import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import { Stack } from "@mui/material";
 import minterLogo from "../public/minterImage.jpeg";
-import { useMoralis } from "react-moralis";
+import Moralis from "moralis";
+import {
+  useApiContract,
+  useMoralis,
+  useMoralisQuery,
+  useMoralisSubscription,
+  useWeb3ExecuteFunction,
+} from "react-moralis";
+import { useNotification } from "web3uikit";
+import MinterContract from "../constants/contractABI/Minter.json";
+
+//fetch data from the blockchain
+//create button for users to mint
+//display data in real time
+
+const MAX_SUPPLY: number = 30;
 
 export default function MintCard() {
+  const [totalSupply, setTotalSupply] = React.useState(0);
+  const [newEvent, setNewEvent] =
+    React.useState<Moralis.Object<Moralis.Attributes>>();
+
   const {
     account,
     enableWeb3,
     isWeb3Enabled,
     isWeb3EnableLoading,
-    Moralis,
     deactivateWeb3,
   } = useMoralis();
 
-  // React.useEffect(() => {
-  //   Moralis.onAccountChanged(async () => {
-  //     await deactivateWeb3();
+  //contract interaction
+  // const { fetch: getTotalSupply, isLoading: isGettingTotalSupply } =
+  //   useWeb3ExecuteFunction({
+  //     abi: MinterContract.abi,
+  //     contractAddress: MinterContract.address,
+  //     functionName: "totalSupply",
   //   });
-  // }, []);
+
+  const { runContractFunction: getTotalSupply, data } = useApiContract({
+    abi: MinterContract.abi,
+    address: MinterContract.address,
+    functionName: "totalSupply",
+    chain: "mumbai",
+  });
+  React.useEffect(() => {
+    (async () => {
+      await getTotalSupply({
+        onSuccess: (results) => {
+          //@ts-ignore
+          console.log(results);
+        },
+      });
+    })();
+  }, []);
+  if (data) console.log(data);
+  const { fetch: mintNfts, isLoading: isMinting } = useWeb3ExecuteFunction({
+    abi: MinterContract.abi,
+    contractAddress: MinterContract.address,
+    functionName: "mintNfts",
+    params: { _amount: 1 },
+    msgValue: Moralis.Units.ETH("0.001"),
+  });
+  const dispatch = useNotification();
+
+  //event listener
+  // useMoralisSubscription("MintedNFTs", (q) => q, [], {
+  //   onUpdate: (data) => {
+  //     console.log(data);
+  //   },
+  // });
+
+  // const { fetch: getTotalSupply } = useMoralisQuery("MintedNFTs", (q) => q, []);
+
+  React.useEffect(() => {
+    (async () => {
+      await getTotalSupply({
+        onSuccess: (results) => console.log(results),
+      });
+    })();
+  }, []);
+
+  const handleMint = async () => {
+    if (isWeb3Enabled && account) {
+      await mintNfts({
+        onSuccess: async (results) => {
+          //@ts-ignore
+          await results.wait(1);
+          dispatch({
+            position: "topR",
+            type: "success",
+            icon: "bell",
+            title: "Minting Successfully",
+            message: "Please verify on Etherscan",
+          });
+        },
+        onError: (error) => {
+          console.log(error);
+          dispatch({
+            position: "topR",
+            type: "error",
+            icon: "bell",
+            title: "Minting Error",
+            message: "Please mint again",
+          });
+        },
+      });
+    }
+  };
 
   return (
     <Card sx={{ maxWidth: 400 }}>
@@ -44,7 +135,7 @@ export default function MintCard() {
           variant="h4"
           color="text.secondary"
         >
-          1 out of 30 Minted
+          {`${totalSupply} out of ${MAX_SUPPLY} MINTED!`}
         </Typography>
         <Typography textAlign="center" variant="body2">
           {account}
@@ -52,32 +143,29 @@ export default function MintCard() {
       </CardContent>
       <CardActions sx={{ paddingBottom: 5 }}>
         <Stack gap={2} direction="row" justifyContent="center" width="100%">
-          {account ? (
-            <Button
-              size="large"
-              onClick={() => {
-                if (isWeb3Enabled) alert("clicked");
-              }}
-            >
-              Mint
-            </Button>
+          {isWeb3Enabled && account ? (
+            <>
+              <Button disabled={isMinting} size="large" onClick={handleMint}>
+                Mint
+              </Button>
+              <Button
+                disabled={isMinting}
+                onClick={async () => {
+                  await deactivateWeb3();
+                }}
+              >
+                Disconnect
+              </Button>
+            </>
           ) : (
             <Button
-              disabled={isWeb3EnableLoading}
-              // disable={isWeb3EnableLoading}
+              disabled={isWeb3EnableLoading || isMinting}
               size="large"
               onClick={async () => await enableWeb3()}
             >
               Connect Wallet
             </Button>
           )}
-          <Button
-            onClick={async () => {
-              await deactivateWeb3();
-            }}
-          >
-            Disconnect
-          </Button>
         </Stack>
       </CardActions>
     </Card>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import darkTheme from "../constants/darkTheme";
+import { NotificationProvider } from "web3uikit";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider initializeOnMount={false}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <NotificationProvider>
+          <Component {...pageProps} />
+        </NotificationProvider>
       </ThemeProvider>
     </MoralisProvider>
   );
